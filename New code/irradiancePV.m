0e06d953ee406ffdd36a0c538bd44d91bbf1dd94
Quintana <dquintan@nrel.gov>
2019-05-30 10:34:48 -0600

    Moved the for loop to calculate irradiance to irradiancePV

--- New code/irradiancePV.m
+++ New code/irradiancePV.m
@@ -1,28 +1,30 @@
-function [irradiance, isTrue] = irradiancePV(j,I,Id,rhoG,beta,gamma,merid,lat,long,Gsc,n)
+function [It] = irradiancePV(I,Id,It,rhoG,beta,gamma,merid,lat,long,Gsc,n,nStates)
 %This function will process solar/weather data to determine irradiance on tilted PV panel.
-% Input: j = jth index (col)
-% Input: I = global irradiance
-% Input: Id = diffuse horizontal irradiance
-% Input: rhoG = albedo
-% Input: beta = tilt angle
-% Input: gamma
-% Input: merid = meridian
-% Input: lat = latitude
-% Input: long = longitude
-% Input: Gsc = solar constant [W/m2]
-% Input: n = day of year for each hour of year [day]
+%   Input: I = global irradiance
+%   Input: Id = diffuse horizontal irradiance
+%   Input: rhoG = albedo
+%   Input: beta = tilt angle
+%   Input: gamma
+%   Input: merid = meridian
+%   Input: lat = latitude
+%   Input: long = longitude
+%   Input: Gsc = solar constant [W/m2]
+%   Input: n = day of year for each hour of year [day]
+%   Input: nStates = number of states
 %
-% Output: irradiance = irradiance on a tilted panel
-% Output: isTrue = 1 or 0 depending if Ai > 1 or Rb > 50
+%   Output: It = irradiance on a tilted panel matrix
 
 hour = repmat((1:24).',365,1);                  %local time for each hour of year [hr]
-isTrue = 0;                                     %1 if Ai > 1 || Rb > 50
+
+for i=1:nStates
+
+for j=1:8760
 
 %--Solar Time and Hour Angles
 
-B = (n-1)*(360/365);                                 %factor for solar time [deg]
+B = (n(j)-1)*(360/365);                                 %factor for solar time [deg]
 E = 229.2*(0.000075 + 0.001868*cosd(B) - 0.032077*sind(B) - 0.014615*cosd(2*B) - 0.04089*sind(2*B));   %factor for solar time
-tSolDiff = 4*(merid-long) + E;                    %difference between standard and solar time [min]
+tSolDiff = 4*(merid(i)-long(i)) + E;                    %difference between standard and solar time [min]
 tSol = hour(j) + tSolDiff/60;                           %solar time [hrs]
 if tSol > 24
     tSol = (tSol-24);                                   %correct solar time if needed
@@ -32,8 +34,8 @@ end
 omegaA = 15*(tSol - 1 - 12);                            %start of hour angle [deg]
 omegaB = 15*(tSol - 12);                                %end of hour angle [deg]
 
-delta = 23.45 * sind(360*(284+n)/365);               %solar declination angle
-omegaSet = acosd(-tand(lat)*tand(delta));            %sunset angle [deg]
+delta = 23.45 * sind(360*(284+n(j))/365);               %solar declination angle
+omegaSet = acosd(-tand(lat(i))*tand(delta));            %sunset angle [deg]
 omegaRise = -omegaSet;                                  %sunrise angle [deg]
 
 if omegaA < omegaRise && omegaB >= omegaRise
@@ -45,11 +47,11 @@ end
 
 %--Rb Factor
 
-aRb = (sind(delta)*sind(lat)*cosd(beta) - sind(delta)*cosd(lat)*sind(beta)*cosd(gamma)) * (omegaB-omegaA)*pi/180 ...
-	+ (cosd(delta)*cosd(lat)*cosd(beta) + cosd(delta)*sind(lat)*sind(beta)*cosd(gamma)) * (sind(omegaB) - sind(omegaA)) ...
-	- (cosd(delta)*sind(beta)*sind(gamma)) * (cosd(omegaB)-cosd(omegaA));
+aRb = (sind(delta)*sind(lat(i))*cosd(beta(i)) - sind(delta)*cosd(lat(i))*sind(beta(i))*cosd(gamma)) * (omegaB-omegaA)*pi/180 ...
+	+ (cosd(delta)*cosd(lat(i))*cosd(beta(i)) + cosd(delta)*sind(lat(i))*sind(beta(i))*cosd(gamma)) * (sind(omegaB) - sind(omegaA)) ...
+	- (cosd(delta)*sind(beta(i))*sind(gamma)) * (cosd(omegaB)-cosd(omegaA));
 
-bRb = (cosd(lat)*cosd(delta))*(sind(omegaB)-sind(omegaA)) + (sind(lat)*sind(delta))*(omegaB-omegaA)*pi/180;
+bRb = (cosd(lat(i))*cosd(delta))*(sind(omegaB)-sind(omegaA)) + (sind(lat(i))*sind(delta))*(omegaB-omegaA)*pi/180;
 Rb = aRb/bRb;                                           %ratio of beam radiation on tilted surface to horizontal surface
 
 if Rb<0                                                 %correct Rb when necessary
@@ -58,36 +60,39 @@ elseif Rb>50
     Rb = 0;
 end
 
+
 %--Incident Irradiance on Tilted Panel
 
-Io = (12*3600/pi)*Gsc*(1+0.033*cosd(360*n/365)) ...
-		* ((cosd(lat)*cosd(delta)*(sind(omegaB)-sind(omegaA))) ...
-		+ pi*(omegaB-omegaA)*sind(lat)*sind(delta)/180);     %extraterrestrial radiation on horizontal surface [W-hr/m2]
+Io = (12*3600/pi)*Gsc*(1+0.033*cosd(360*n(j)/365)) ...
+		* ((cosd(lat(i))*cosd(delta)*(sind(omegaB)-sind(omegaA))) ...
+		+ pi*(omegaB-omegaA)*sind(lat(i))*sind(delta)/180);     %extraterrestrial radiation on horizontal surface [W-hr/m2]
     
-Ib = I - Id;                              %beam horizontal irradiance [W-hr/m2]
+Ib = I(j,i) - Id(j,i);                              %beam horizontal irradiance [W-hr/m2]
 if Ib<0
     Ib=0;                                           %correct Ib for instances when TMY3 model overpredicts Id
 end
 
 Ai = Ib/Io;                                         %anisotropy index
-f = sqrt(Ib/I);                                %modulating factor
-if I==0
+f = sqrt(Ib/I(j,i));                                %modulating factor
+if I(j,i)==0
     f=0;
 end
 
-irradiance = (Ib + Id*Ai)*Rb ...
-        + Id*(1-Ai)*((1+cosd(beta))/2)*(1+f*(sind(beta/2))^3) ...
-        + I*rhoG*((1-cosd(beta))/2);   %irradiance on tilted plate [W-hr/m2]
+It(j,i) = (Ib + Id(j,i)*Ai)*Rb ...
+        + Id(j,i)*(1-Ai)*((1+cosd(beta(i)))/2)*(1+f*(sind(beta(i)/2))^3) ...
+        + I(j,i)*rhoG(j,i)*((1-cosd(beta(i)))/2);   %irradiance on tilted plate [W-hr/m2]
 
 if omegaB < omegaRise                               %set irradiance to zero at night
-    irradiance = 0;
+    It(j,i) = 0;
 elseif omegaA > omegaSet
-    irradiance = 0;
+    It(j,i) = 0;
 end
 if Ai > 1 || Rb > 50
-    irradiance = 0;
-    isTrue = 1;
+    It(j,1) = 0;
+end
+    
+
+end
 end
 
 end
-
